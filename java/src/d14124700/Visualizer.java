d512e754a0a31e44e1fbab5ad868ab2cd2fa999e
Dylan <dylanconvery94@gmail.com>
2020-05-15 14:46:52 +0100

    initial setup

---
+++ java/src/d14124700/Visualizer.java
@@ -0,0 +1,11 @@
+package d14124700;
+
+import ie.tudublin.Visual;
+import ie.tudublin.VisualException;
+
+public class Visualizer extends Visual {
+    public void settings(){};
+    public void setup(){};
+    public void keyPressed(){};
+    public void draw(){}
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,21 +1,10 @@
 package ie.tudublin;
 
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
+import d14124700.Visualizer;
 
-public class Main
-{	
-
-	public void startUI()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
-	}
-
-	public static void main(String[] args)
-	{
-		Main main = new Main();
-		main.startUI();			
-	}
+public class Main {
+    public static void main(String[] args) {
+        String[] processing_args = {"MAIN"};
+        processing.core.PApplet.runSketch(processing_args, new Visualizer());
+    }
 }
