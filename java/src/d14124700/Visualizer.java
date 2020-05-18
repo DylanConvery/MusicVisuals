f93b737d6de6381a6198bd36d1a06a05a8ae0f9e
Dylan <dylanconvery94@gmail.com>
2020-05-18 01:58:22 +0100

    readded rotation

--- java/src/d14124700/Visualizer.java
+++ java/src/d14124700/Visualizer.java
@@ -18,14 +18,14 @@ public class Visualizer extends Visual {
     float y2;
 
     public void settings() {
-        //size(1000, 1000);
+        // size(1000, 1000);
         fullScreen();
     }
 
     public void setup() {
         circle_x = width / 2;
         circle_y = height / 2;
-        circle_width = width /5;
+        circle_width = width / 5;
         circle_radius = circle_width / 2f;
         startMinim();
         loadAudio("heroplanet.mp3");
@@ -37,33 +37,91 @@ public class Visualizer extends Visual {
 
     }
 
+    float angle = 0;
+
     public void draw() {
         background(0);
         colorMode(HSB);
+
+        calculateAverageAmplitude();
+        calculateFrequencyBands();
         try {
             calculateFFT();
         } catch (VisualException e) {
             e.printStackTrace();
         }
 
-        float theta = TWO_PI / (float) spines;
         strokeWeight(5);
-        calculateFrequencyBands();
+        float theta = TWO_PI / (float) spines;
         float[] bands = getSmoothedBands();
         // println("x1 : " + x1 + "\ny1 : " + y1 + "\nx2 : " + x2 + "\ny2 : " + y2);
 
+        translate(circle_x, circle_y);
+        rotate(angle);
+        pushMatrix();
+
         for (int i = 0; i < spines; i++) {
-            
             stroke(map(i, 0, spines, 0, 255), 255, 255);
             float band = bands[i % bands.length];
 
-            x1 = circle_x + (circle_radius * sin(i * theta));
-            y1 = circle_y - (circle_radius * cos(i * theta));
+            x1 = (circle_radius * sin(i * theta));
+            y1 = (circle_radius * cos(i * theta));
 
-            x2 = circle_x + ((circle_radius + band * 0.2f) * sin(i * theta));
-            y2 = circle_y - ((circle_radius + band * 0.2f) * cos(i * theta));
+            x2 = ((circle_radius + band * 0.2f) * sin(i * theta));
+            y2 = ((circle_radius + band * 0.2f) * cos(i * theta));
+
 
             line(x1, y1, x2, y2);
+
         }
+        
+        popMatrix();
+
+        noFill();
+        strokeWeight(1);
+        ellipse(0,0, circle_width, circle_width);
+
+        angle +=getSmoothedAmplitude() / 8.0f;
     }
 }
+
+// calculateAverageAmplitude();
+// try {
+// calculateFFT();
+// } catch (VisualException e) {
+// e.printStackTrace();
+// }
+
+// float cgap = 255 / 100 + 1;
+
+// background(0);
+// colorMode(HSB);
+// strokeWeight(5);
+
+// pushMatrix();
+
+// stroke(100, 255, 255);
+// translate(circle_x, circle_y);
+// rotate(ang);
+
+// int lines = 100;
+// float angle = TWO_PI / (float) lines;
+// ang += getSmoothedAmplitude() / 8.0f;
+
+// for (int i = 0; i < lines; i++) {
+// float c = ((i + offs) * cgap) % 255;
+// stroke(c, 255, 255);
+
+// float x1 = (circle_width) / 2 * sin(angle * i);
+// float y1 = (circle_width) / 2 * cos(angle * i);
+// float x2 = (circle_width + 100) / 2 * sin(angle * i);
+// float y2 = (circle_width + 100) / 2 * cos(angle * i);
+// line(x1, y1, x2, y2);
+// }
+
+// popMatrix();
+
+// strokeWeight(20);
+// noFill();
+// stroke(255, 255, 255);
+// ellipse(circle_y, circle_y, circle_width, circle_width);
