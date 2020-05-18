35008f436bac24b0cb4692560794229ff3ef11df
Dylan <dylanconvery94@gmail.com>
2020-05-18 01:22:34 +0100

    have spines reacting with audio bands

--- java/src/d14124700/Visualizer.java
+++ java/src/d14124700/Visualizer.java
@@ -6,25 +6,31 @@ import ie.tudublin.VisualException;
 public class Visualizer extends Visual {
     float circle_x;
     float circle_y;
-    float circle_width = 500;
-    float circle_radius = circle_width / 2f;
+    float circle_width;
+    float circle_radius;
 
-    float angle = 0;
-    int numSamples = 100;
-    float[] lerpedSamples = new float[numSamples];
-    float offs = 0;
+    int spines = 200;
+    float[] lerpedSamples = new float[spines];
+
+    float x1;
+    float y1;
+    float x2;
+    float y2;
 
     public void settings() {
-        size(1000, 1000);
+        //size(1000, 1000);
+        fullScreen();
     }
 
     public void setup() {
-        setFrameSize(1024);
         circle_x = width / 2;
         circle_y = height / 2;
+        circle_width = width /5;
+        circle_radius = circle_width / 2f;
         startMinim();
         loadAudio("heroplanet.mp3");
         getAudioPlayer().play();
+        println(getSmoothedBands().length);
     }
 
     public void keyPressed() {
@@ -40,37 +46,24 @@ public class Visualizer extends Visual {
             e.printStackTrace();
         }
 
-        float theta = TWO_PI / (float) numSamples;
-        float average = 0;
-
+        float theta = TWO_PI / (float) spines;
         strokeWeight(5);
-        stroke(100, 255, 255);
-        for (int i = 0; i < numSamples; i++) {
-            lerpedSamples[i] = lerp(lerpedSamples[i], fft.getBand(i), 0.2f);
+        calculateFrequencyBands();
+        float[] bands = getSmoothedBands();
+        // println("x1 : " + x1 + "\ny1 : " + y1 + "\nx2 : " + x2 + "\ny2 : " + y2);
 
+        for (int i = 0; i < spines; i++) {
+            
+            stroke(map(i, 0, spines, 0, 255), 255, 255);
+            float band = bands[i % bands.length];
 
-            stroke(map(i, 0, numSamples, 0, 255), 255, 255);
+            x1 = circle_x + (circle_radius * sin(i * theta));
+            y1 = circle_y - (circle_radius * cos(i * theta));
 
-            float x1 = circle_x + (circle_radius * sin(i * theta));
-            float y1 = circle_y - (circle_radius * cos(i * theta));
-            float x2 = circle_x + ((lerpedSamples[i] * circle_radius) * sin(i * theta));
-            float y2 = circle_y - ((lerpedSamples[i] * circle_radius) * cos(i * theta));
-            println("x2 : " + x2 + "\ny2 : " + y2);
+            x2 = circle_x + ((circle_radius + band * 0.2f) * sin(i * theta));
+            y2 = circle_y - ((circle_radius + band * 0.2f) * cos(i * theta));
+
             line(x1, y1, x2, y2);
         }
-
-
-        strokeWeight(20);
-        stroke(255, 255, 255);
-        noFill();
-        // ellipse(circle_y, circle_y, circle_width, circle_width);
-
     }
 }
-
-// x = rsin(θ), y = rcos(θ).
-
-// float x1 = (circle_width) / 2 * sin(angle * i);
-// float y1 = (circle_width) / 2 * cos(angle * i);
-// float x2 = (circle_width + 100) / 2 * sin(angle * i);
-// float y2 = (circle_width + 100) / 2 * cos(angle * i);
